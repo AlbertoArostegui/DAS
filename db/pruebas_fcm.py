5b5959dcd98df247f8f67af4ea922bec16760353
Alberto Aróstegui <74300793+AlbertoArostegui@users.noreply.github.com>
2024-04-20 18:57:27 +0200

    Update pruebas_fcm.py

--- db/pruebas_fcm.py
+++ db/pruebas_fcm.py
@@ -1,10 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate("C:\\Users\\alberto\\Downloads\\entrega-segunda-firebase-adminsdk-0ohhs-c7b7877573.json")
+cred = credentials.Certificate(".json")
 firebase_admin.initialize_app(cred)
 
-registration_token = "flyNv2m9RDG2aM1tlchno_:APA91bEGsFtcjBicFBmfYH9wiRJgagzWtkauhNC7fO_BE0AMRThf-PNrLAzLESA8PoCbvzFNf-5V8i01xGtbD8yFwwZRHn_N9ChE-FcGgCMXCKSfZqLy13xCrXgEWxfJK7mDGMr7HhUK"
+registration_token = ""
 
 # Construct a message payload
 message = messaging.Message(
