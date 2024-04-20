b9a09f0186f5e0333b5f41fd8b581544df77f3c3
AlbertoArostegui <aarosteguig@gmail.com>
2024-04-20 17:26:14 +0200

    Notification on Firebase Message

--- app/src/main/java/com/example/entrega_primera/ServicioFirebase.java
+++ app/src/main/java/com/example/entrega_primera/ServicioFirebase.java
@@ -1,10 +1,17 @@
 package com.example.entrega_primera;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -35,6 +42,7 @@ public class ServicioFirebase extends FirebaseMessagingService {
         }
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+            showNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
         }
     }
 
@@ -43,5 +51,26 @@ public class ServicioFirebase extends FirebaseMessagingService {
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString("fcm_token", token);
         editor.apply();
+    }
+
+    private void showNotification(String title, String message) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
+
+        String channelId = "Default";
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(message).setAutoCancel(true).setContentIntent(pendingIntent);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId, "Default channel", NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
+        }
+
+        manager.notify(0, builder.build());
     }
 }
